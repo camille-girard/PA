3a12078f2a09be55a690fc7d24ab52b73060851f
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-04-17 13:28:21 +0200

    feat(backend:security): implement logout functionality in SecurityController

---
+++ backend/src/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api')]
+final class SecurityController extends AbstractController
+{
+    #[Route('/logout', name: 'app_security')]
+    public function index(Request $request, EventDispatcherInterface $eventDispatcher, TokenStorageInterface $tokenStorage): JsonResponse
+    {
+        $eventDispatcher->dispatch(new LogoutEvent($request, $tokenStorage->getToken()));
+
+        return new JsonResponse();
+    }
+}
