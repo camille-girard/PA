790a9cd7aab0f4a7fe44521ff9ed9eee32e8f3fd
Philippe Delente <philippe.delente@gmail.com>
2025-04-17 22:20:53 +0200

    add compose.prod + add ci/cd lint and deployement

---
+++ backend/.php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'yoda_style' => true,
+    ])
+    ->setFinder($finder)
+;
--- backend/src/Controller/SecurityController.php
+++ backend/src/Controller/SecurityController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
-use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api')]
 final class SecurityController extends AbstractController
