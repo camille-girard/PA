6c66668cdeab463294b87e173c705601608626da
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-04-20 18:36:46 +0200

    feat(backend:refresh-token): add initial refresh token system

---
+++ backend/src/Entity/RefreshToken.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RefreshTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: RefreshTokenRepository::class)]
+class RefreshToken
+{
+    #[ORM\Id]
+    #[ORM\Column(length: 36)]
+    private string $id;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $token = null;
+
+    #[ORM\Column]
+    private ?\DateTime $expiresAt = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v7()->toRfc4122();
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): static
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getExpiresAt(): ?\DateTime
+    {
+        return $this->expiresAt;
+    }
+
+    public function setExpiresAt(\DateTime $expiresAt): static
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
