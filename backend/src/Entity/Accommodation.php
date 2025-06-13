3085c5ea9aebdc7be45d00489ec19eb5a005108c
cgirard <camille.girard1995@gmail.com>
2025-06-13 14:02:43 +0200

    add map

--- backend/src/Entity/Accommodation.php
+++ backend/src/Entity/Accommodation.php
@@ -14,9 +14,6 @@ class Accommodation
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    /**
-     * @phpstan-ignore-next-line
-     */
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
@@ -34,9 +31,6 @@ class Accommodation
     #[ORM\Column]
     private ?float $price = null;
 
-    /**
-     * @var array<string>
-     */
     #[ORM\Column(type: Types::ARRAY)]
     private array $adventage = [];
 
@@ -53,23 +47,20 @@ class Accommodation
     #[ORM\ManyToOne(inversedBy: 'accommodations')]
     private ?Theme $theme = null;
 
-    /**
-     * @var Collection<int, AccommodationImages>
-     */
     #[ORM\OneToMany(targetEntity: AccommodationImages::class, mappedBy: 'accommodation')]
     private Collection $images;
 
-    /**
-     * @var Collection<int, Booking>
-     */
     #[ORM\OneToMany(targetEntity: Booking::class, mappedBy: 'accommodation', orphanRemoval: true)]
     private Collection $bookings;
 
-    /**
-     * @var Collection<int, Comment>
-     */
     #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'accommodation', orphanRemoval: true)]
     private Collection $comments;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $latitude = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $longitude = null;
 
     public function __construct()
     {
@@ -91,7 +82,6 @@ class Accommodation
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
@@ -103,7 +93,6 @@ class Accommodation
     public function setDescription(?string $description): static
     {
         $this->description = $description;
-
         return $this;
     }
 
@@ -115,7 +104,6 @@ class Accommodation
     public function setAddress(string $address): static
     {
         $this->address = $address;
-
         return $this;
     }
 
@@ -127,7 +115,6 @@ class Accommodation
     public function setCapacity(int $capacity): static
     {
         $this->capacity = $capacity;
-
         return $this;
     }
 
@@ -139,25 +126,17 @@ class Accommodation
     public function setPrice(float $price): static
     {
         $this->price = $price;
-
         return $this;
     }
 
-    /**
-     * @return array<string>
-     */
     public function getAdventage(): array
     {
         return $this->adventage;
     }
 
-    /**
-     * @param array<string> $adventage
-     */
     public function setAdventage(array $adventage): static
     {
         $this->adventage = $adventage;
-
         return $this;
     }
 
@@ -169,7 +148,6 @@ class Accommodation
     public function setPracticalInformations(string $practicalInformations): static
     {
         $this->practicalInformations = $practicalInformations;
-
         return $this;
     }
 
@@ -181,7 +159,6 @@ class Accommodation
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
-
         return $this;
     }
 
@@ -193,7 +170,6 @@ class Accommodation
     public function setOwner(?Owner $owner): static
     {
         $this->owner = $owner;
-
         return $this;
     }
 
@@ -205,13 +181,9 @@ class Accommodation
     public function setTheme(?Theme $theme): static
     {
         $this->theme = $theme;
-
         return $this;
     }
 
-    /**
-     * @return Collection<int, AccommodationImages>
-     */
     public function getImages(): Collection
     {
         return $this->images;
@@ -230,7 +202,6 @@ class Accommodation
     public function removeImage(AccommodationImages $image): static
     {
         if ($this->images->removeElement($image)) {
-            // set the owning side to null (unless already changed)
             if ($image->getAccommodation() === $this) {
                 $image->setAccommodation(null);
             }
@@ -239,9 +210,6 @@ class Accommodation
         return $this;
     }
 
-    /**
-     * @return Collection<int, Booking>
-     */
     public function getBookings(): Collection
     {
         return $this->bookings;
@@ -260,7 +228,6 @@ class Accommodation
     public function removeBooking(Booking $booking): static
     {
         if ($this->bookings->removeElement($booking)) {
-            // set the owning side to null (unless already changed)
             if ($booking->getAccommodation() === $this) {
                 $booking->setAccommodation(null);
             }
@@ -269,9 +236,6 @@ class Accommodation
         return $this;
     }
 
-    /**
-     * @return Collection<int, Comment>
-     */
     public function getComments(): Collection
     {
         return $this->comments;
@@ -290,12 +254,33 @@ class Accommodation
     public function removeComment(Comment $comment): static
     {
         if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
             if ($comment->getAccommodation() === $this) {
                 $comment->setAccommodation(null);
             }
         }
 
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(?float $latitude): static
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(?float $longitude): static
+    {
+        $this->longitude = $longitude;
         return $this;
     }
 }
