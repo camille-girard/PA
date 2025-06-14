b3897bbb8077b6ed5b0f2106ed7b5b91a7499f5f
camille-girard <139240684+camille-girard@users.noreply.github.com>
2025-06-14 12:32:24 +0200

    Feat/map (#28)
    * add map
    
    * feat(backend): add map
    
    * feat(backend): upd style map

--- frontend/pages/explorer.vue
+++ frontend/pages/explorer.vue
@@ -1,23 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MapboxMap from "~/components/MapBox.vue";
+</script>
 
 <template>
-    <main>
-        <UHeader />
-        <div class="max-w-7xl w-full mx-auto pt-8 px-4">
-            <section class="w-full pt-8">
-                <div class="py-20 rounded-2xl flex items-center justify-center relative">
-                    <div class="text-center z-10">
-                        <h1 class="text-h1">Explorez votre prochaine destination</h1>
-                        <p class="mt-4">Découvrez le lieu de vos prochaines vacances</p>
-                    </div>
-                </div>
-            </section>
-            <section class="w-full pt-12">
-                <div class="flex justify-center">
-                    <img src="/Map.png" alt="Carte des lieux à Paris" class="rounded-2xl max-w-full" />
-                </div>
-            </section>
+  <main>
+    <UHeader />
+    <div class="max-w-7xl w-full mx-auto pt-8 px-4">
+      <section class="w-full pt-8">
+        <div class="py-20 rounded-2xl flex items-center justify-center relative">
+          <div class="text-center z-10">
+            <h1 class="text-h1">Explorez votre prochaine destination</h1>
+            <p class="mt-4">Découvrez le lieu de vos prochaines vacances</p>
+          </div>
         </div>
-        <UFooter />
-    </main>
+      </section>
+
+      <section class="w-full pt-12">
+        <div class="h-[500px] w-full overflow-hidden rounded-xl shadow-lg">
+          <ClientOnly>
+            <MapboxMap />
+          </ClientOnly>
+        </div>
+      </section>
+    </div>
+    <UFooter />
+  </main>
 </template>
