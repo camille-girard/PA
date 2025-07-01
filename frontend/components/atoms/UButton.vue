f760d7d8a3e5a8d9a1d769129af891386b71acb6
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2025-07-01 22:00:23 +0200

    fix: refine button styles for better alignment and consistency

--- frontend/components/atoms/UButton.vue
+++ frontend/components/atoms/UButton.vue
@@ -22,7 +22,7 @@
     }>();
 
     const baseClasses =
-        'text-body-md flex items-center font-semibold gap-1 rounded-lg transition focus:ring-1 focus:ring-primary ring-offset-2 focus:outline-none ring-offset-transparent [&_svg]:size-5';
+        'flex items-center justify-center font-semibold gap-1 rounded-lg transition focus:ring-1 focus:ring-primary ring-offset-2 focus:outline-none ring-offset-transparent [&_svg]:size-5 [&_svg]:transition-colors [&_svg]:duration-200 [&_svg]:ease-in-out text-body-md';
 
     const sizeClasses = {
         sm: 'px-3 py-2 text-sm',
@@ -33,11 +33,10 @@
 
     const variantClasses = {
         primary:
-            'justify-center bg-brand-solid hover:bg-brand-solid-hover text-white enabled:shadow-xs-custom border border-brand enabled:bg-linear-grad disabled:border-disabled-subtle disabled:bg-disabled disabled:text-fg-disabled [&_svg]:text-button-primary-icon [&_svg]:hover:text-button-primary-icon-hover',
+            'bg-brand-solid hover:bg-brand-solid-hover text-white enabled:shadow-xs-custom border border-brand enabled:bg-linear-grad disabled:border-disabled-subtle disabled:bg-disabled disabled:text-fg-disabled [&_svg]:text-button-primary-icon [&_svg]:hover:text-button-primary-icon-hover',
         secondary:
-            'justify-center text-secondary hover:text-secondary-hover shadow-xs-skeuomorphic disabled:text-fg-disabled disabled:border-disabled-subtle [&_svg]:text-fg-quaternary [&_svg]:hover:text-fg-quaternary-hover',
-        tertiary:
-            'justify-center text-tertiary enabled:hover:bg-primary-hover hover:text-tertiary-hover disabled:text-fg-disabled',
+            'bg-primary enabled:hover:bg-primary-hover text-secondary hover:text-secondary-hover border border-primary disabled:text-fg-disabled disabled:border-disabled-subtle [&_svg]:text-fg-quaternary [&_svg]:hover:text-fg-quaternary-hover shadow-xs',
+        tertiary: 'text-tertiary enabled:hover:bg-primary-hover hover:text-tertiary-hover disabled:text-fg-disabled',
         transparent:
             'justify-start bg-transparent text-gray-700 hover:text-orange-600 text-left px-4 py-2 w-full font-medium [&_svg]:text-gray-700 [&_svg]:hover:text-orange-600',
         danger: 'justify-center bg-red-100 hover:bg-red-200 text-red-700 border border-red-300 font-semibold shadow-sm disabled:opacity-50',
