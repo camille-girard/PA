b5b2d9ab7533ff75d98c56be3f5c2a2fdc0ebe28
Philippe DELENTE <80407535+PhilDaiguille@users.noreply.github.com>
2025-07-20 11:51:26 +0200

    fix env matoto url (#74)

--- frontend/plugins/vue-matomo.client.ts
+++ frontend/plugins/vue-matomo.client.ts
@@ -3,7 +3,7 @@ import VueMatomo from 'vue-matomo';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueMatomo, {
-        host: 'http://localhost:8082',
+        host: import.meta.env.MATOMO_URL || 'http://localhost:8082',
         siteId: 1,
     });
 });
