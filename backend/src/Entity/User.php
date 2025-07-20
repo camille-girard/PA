ce73b4b60e3646913e854ab484bda553673fbaf5
Philippe Delente <philippe.delente@gmail.com>
2025-07-20 22:15:45 +0200

    fix problem role

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -140,7 +140,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_CLIENT';
 
         return array_unique($roles);
     }
