2fcc3cf9f520291554240ee48c852e44300f0da2
Philippe DELENTE <80407535+PhilDaiguille@users.noreply.github.com>
2025-07-20 22:21:50 +0200

    fix problem matomo frontend (#81)

--- frontend/plugins/vue-matomo.client.ts
+++ frontend/plugins/vue-matomo.client.ts
@@ -2,8 +2,9 @@ import { defineNuxtPlugin } from '#app';
 import VueMatomo from 'vue-matomo';
 
 export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig();
     nuxtApp.vueApp.use(VueMatomo, {
-        host: import.meta.env.MATOMO_URL || 'http://localhost:8082',
+        host: config.public.matomoUrl  || 'http://localhost:8082',
         siteId: 1,
     });
 });
