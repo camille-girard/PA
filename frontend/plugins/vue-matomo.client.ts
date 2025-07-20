8eb7abfbb97f51885184d449da821b4ecb5ef2b3
Philippe DELENTE <80407535+PhilDaiguille@users.noreply.github.com>
2025-07-20 22:51:50 +0200

    fix problem client (#82)

--- frontend/plugins/vue-matomo.client.ts
+++ frontend/plugins/vue-matomo.client.ts
@@ -4,7 +4,7 @@ import VueMatomo from 'vue-matomo';
 export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig();
     nuxtApp.vueApp.use(VueMatomo, {
-        host: config.public.matomoUrl  || 'http://localhost:8082',
+        host: config.public.matomoUrl  || 'https://matomo.popnbed.com',
         siteId: 1,
     });
 });
